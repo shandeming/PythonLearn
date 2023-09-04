6f7e86eb6a1944ae854e498ad5c836731f952e74
shandeming <2962847300@qq.com>
2023-09-04 12:10:50 +0800

    origin

---
+++ FirstDay.py
@@ -0,0 +1,7 @@
+import math
+def calculateSinCosTan(x):
+  #write your function here
+  sine =math.sin(x)  0#calculate sine
+  cos =math.cos(x) 0#calculate cosine
+  tan =math.tan(x) 0#calculate tangent 
+  return [sine, cos, tan]
